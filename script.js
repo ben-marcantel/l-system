99535cd51efc0c6c86bf36ebff0ef1aa230c6dc4
MyMac <mymac@mymacs-mbp.attlocal.net>
2018-01-18 09:12:03 -0600

    draw logic almost finished

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const button = document.getElementById('button');
+
 let currentString = "X";
 let nextString = "";
+
+
+
 
 
 let generate = ()=>{
@@ -18,44 +25,80 @@ let generate = ()=>{
 };
 
 
-let draw = ()=>{
-    c.beginPath();    
-    c.font = "1em Futura";
-    c.strokeText(currentString,0,100);
-    c.strokeStyle = "red";
-    c.stroke();
-    c.fillStyle = 'rgb(0,0,0)';
+let interpret = ()=>{
+    for (let i =0; i<currentString.length; i++){
+        let decoder = currentString.charAt(i);
+        
+        let x = 0;
+        let y = 100;
+        let newX = x;
+        let newY = y;
+        let currentX = "";
+        let currentY = "";
+        
+        if (decoder == "F"){ 
+            // F means "draw forward
+            c.restore(); 
+            c.lineTo(newX,newY);
+            c.save();
+
+            // c.closePath();
+            
+        } else if (decoder == "X"){
+            // X does not correspond to any drawing action and is used to control the evolution of the curve. 
+            
+            c.rotate((i * Math.PI)/ 180);
+            c.closePath();
+
+        } else if (decoder == "+"){
+            // − means "turn left 25°"
+            newX = x;
+            newY = y + (y/2);
+            c.lineTo(newX,newY);
+            c.closePath();
+            c.rotate(-(45 * Math.PI / 180));
+            
+        } else if (decoder == "-"){
+            // + means "turn right 25°"
+           
+            newX = x;
+            newY = y + (y/2);
+            c.lineTo(newX,newY);
+            c.closePath();
+            c.rotate(45 * Math.PI / 180);
+            
+            
+        } else if (decoder == "["){
+            // The square bracket "[" corresponds to saving the current values for position and angle,
+            // c.save();
+        
+        } else (decoder == "]")
+        // which are restored when the corresponding "]" is executed
+            c.restore(); 
+            
+    }   
 };
 
 
-let interpret = ()=>{
-    for (let i =0; i<currentString.length; i++){
-        let decode = currentString.charAt(i);
-        if (decode == "F"){
-
-        } else if (decode == "X"){
-
-        } else if (decode == "+"){
-
-        } else if (decode == "-"){
-
-        } else if (decode == "["){
-
-        } else (decode == "]")
-
-    }
+let draw = ()=>{
+    console.log(currentString);
+    c.beginPath();
+    // c.lineTo(canvas.width/2,canvas.height/2);
+    c.translate(canvas.width/2,canvas.height/2);   
+    interpret();
+    c.strokeStyle = "rgb(255,0,200)";
+    c.stroke();
 };
 
 
 let magic = ()=>{
-    for (let i=0; i<3;i++){
+    for (let i=0; i<1;i++){
         generate(); 
         draw();
     };
 };
 
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-button.addEventListener("click",magic);
+button.addEventListener("click", magic);
+
  
